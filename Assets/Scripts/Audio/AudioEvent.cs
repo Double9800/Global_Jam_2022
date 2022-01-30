416f4d358f420acb857be2607b81a4e9ee77303f
Tommaso <tommybo99@gmail.com>
2022-01-30 13:55:33 +0100

    Added every sound except Intereface

--- Assets/Scripts/Audio/AudioEvent.cs
+++ Assets/Scripts/Audio/AudioEvent.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class AudioEvent : MonoBehaviour
 {
-    public void Walk()
+    public void Walk1()
     {
-        AudioManager.instance.Play("Walk");
+        AudioManager.instance.Play("Walk1");
     }
-
+    public void Walk2()
+    {
+        AudioManager.instance.Play("Walk2");
+    }
+    public void Walk3()
+    {
+        AudioManager.instance.Play("Walk3");
+    }
+    public void Walk4()
+    {
+        AudioManager.instance.Play("Walk4");
+    }
 }
